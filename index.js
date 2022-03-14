fb7ad09adc05c9c8f11ac59b5792a15e7820e998
Rahul Agarwal <agarwalr327@gmail.com>
2022-03-14 12:50:47 +0530

    Added CSS to delete button

--- index.js
+++ index.js
@@ -13,31 +13,32 @@ async function get_meme(){
             let parent=document.getElementById("parent");
             parent.appendChild(div);
             div.className = "my_div";
+
+            let br=document.createElement("br");
             
             let input = document.createElement('input');
             input.type = 'checkbox'
             input.id = "my_input"
 
-            div.append(img,input);
+            div.append(img,br,input);
 
             input.addEventListener('click',function(){
                 if(this.checked){
-                    this.parentElement.style.backgroundColor = "blue"
-                    // var span = document.createElement('span');
-                    // var text = document.createTextNode("\u00D7");
-                    // span.className = "close";
-        
-                    // span.appendChild(text);
-                    // div.appendChild(span);
+                    this.parentElement.style.backgroundColor = "blue";
                
                     // Delete button 
                     let btn_1=document.createElement("button");
+                    btn_1.className="delete";
                     btn_1.innerHTML="Delete";
                     div.appendChild(btn_1);
+                    btn_1.addEventListener('click',function(){
+                        this.parentElement.style.display = 'none';;
+                    }) 
                     
                     // Favourite button
                     let btn_2=document.createElement("button");
                     btn_2.innerHTML="Favourite";
+                    btn_1.className="favourite";
                     div.appendChild(btn_2);
 
         
