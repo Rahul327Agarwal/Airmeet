217039215e309f0bfb669b6d7da896b1043bcd88
Rahul Agarwal <agarwalr327@gmail.com>
2022-03-14 11:15:38 +0530

    added checkbox

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const upper=document.getElementById("upper");
 
-
-
-async function get_meme()
-{
+async function get_meme(){
     const response=await fetch("https://www.reddit.com/r/memes.json");
     const body=await response.json();
 
@@ -22,43 +19,28 @@ async function get_meme()
             let input = document.createElement('input');
             input.type = 'checkbox'
             input.id = "my_input"
-            // input.onchange = graduatedFunction();
-            // input.addEventListener("onchange", graduatedFunction);
 
             div.append(img,input);
 
-            // var close_button = document.getElementById('my_input');
-            // for(var j=0;j<close_button.length;j++){
-            //     close_button[j].onclick = function(){
-            //         console.log("sds")
-            //         var but = this.parentElement;
-            //         but.style.display = 'none';
-            //     }
-            // }
-
-            // document.getElementById('my_input').change = function(){
-            //     console.log("Ina ansd")
-            // }
-
-            var span = document.createElement('span');
-            var text = document.createTextNode("\u00D7");
-            span.className = "close";
-            
-            span.appendChild(text);
-            div.appendChild(span);
-        
-            
-            img.addEventListener('click',function(){
-                console.log("Ina ansd")
-                if(this.parentElement.style.backgroundColor != "blue"){
+            input.addEventListener('click',function(){
+                if(this.checked){
                     this.parentElement.style.backgroundColor = "blue"
+                    var span = document.createElement('span');
+                    var text = document.createTextNode("\u00D7");
+                    span.className = "close";
+        
+                    span.appendChild(text);
+                    div.appendChild(span);
+        
+                    span.addEventListener('click',function(){
+                        this.parentElement.style.display = 'none';;
+                    })   
                 }
+                
                 else{
                     this.parentElement.style.backgroundColor = "white"
                 }
             })
-
-
         } 
     }
 }
